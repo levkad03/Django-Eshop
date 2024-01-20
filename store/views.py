f60d5d680d9748c2d5cccfd9e221e3a63a5ec650
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-20 14:31:22 +0200

    changed home.html, refactored base.html

--- store/views.py
+++ store/views.py
@@ -5,3 +5,8 @@ from .models import Category, Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
