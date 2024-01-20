0f21cac3e1eda78727edc818b740fd92e0751444
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-20 14:49:00 +0200

    added product detail page

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
@@ -6,7 +6,13 @@ def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
 
+
 def categories(request):
     return {
         'categories': Category.objects.all()
     }
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
