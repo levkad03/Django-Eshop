0d9f216c30b6069a166796b1a83fe142e0324438
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-22 14:26:27 +0200

    added filter by category page

--- store/models.py
+++ store/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
     def __str__(self):
         return self.name
 
