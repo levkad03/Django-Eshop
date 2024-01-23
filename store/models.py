36ff1b2d15444cdc478a5f4c367f521dec9726f7
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-23 15:14:52 +0200

    added custom object manager

--- store/models.py
+++ store/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.query import QuerySet
 from django.urls import reverse
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
 
 
 class Category(models.Model):
@@ -34,6 +40,8 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
+    objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = "Products"
