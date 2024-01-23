549d0a9f4e048a96bf898102becce09502f1cefc
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-23 14:57:14 +0200

    reconfigured context_processor for store

--- store/views.py
+++ store/views.py
@@ -8,12 +8,6 @@ def all_products(request):
     return render(request, 'store/home.html', {'products': products})
 
 
-def categories(request):
-    return {
-        'categories': Category.objects.all()
-    }
-
-
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     return render(request, 'store/products/detail.html', {'product': product})
