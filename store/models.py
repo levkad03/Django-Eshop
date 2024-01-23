3ee36a5fdf88463b3f2485812e013a9d59d00883
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-23 16:24:19 +0200

    changed a picture

--- store/models.py
+++ store/models.py
@@ -33,7 +33,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to=('images/'), default='images/book_default.png')
+    image = models.ImageField(upload_to=('images/'), default='images/book_default.jpg')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
