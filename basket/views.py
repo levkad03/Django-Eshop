b54dfe7a37d6a8c39ac8ff276ac44bc676afa33d
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-27 15:52:21 +0200

    added view for the add function

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .basket import Basket
+from django.shortcuts import get_object_or_404
+from store.models import Product
+
 
 def basket_summary(request):
     return render(request, 'store/basket/summary.html')
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
