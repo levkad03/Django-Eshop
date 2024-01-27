c0043f7077ee4d9bffe90ec5250302091ea62116
Lev Kadatscky <levkadatscky@gmail.com>
2024-01-27 15:07:28 +0200

    added basket class, added context preprocessor for basket

--- core/settings.py
+++ core/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "store.context_processors.categories",
+                "basket.context_processors.basket",
             ],
         },
     },
