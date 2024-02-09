4adfbc1172963e3f2ea60588b6df7e5395e050ff
Lev Kadatskyi <levkadatscky@gmail.com>
2024-02-09 12:47:25 +0200

    added forms.py, created RegistrationForm class

---
+++ account/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import UserBase
+
+
+class RegistrationForm(forms.ModelForm):
+
+    user_name = forms.CharField(label='Enter username', min_length=4, max_length=50, help_text='Required')
+    email = forms.EmailField(max_length=100, help_text='Required', error_messages={
+        'Required': 'Sorry, you will need an email'
+    })
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = UserBase
+        fields = ('user_name', 'email')
