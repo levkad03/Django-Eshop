8c8dc86d37f285bcfe112f9529805aa44fec6ee7
Lev Kadatskyi <levkadatscky@gmail.com>
2024-02-12 14:27:58 +0200

    added login form

--- account/urls.py
+++ account/urls.py
@@ -3,12 +3,16 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserLoginForm
 
 app_name = 'account'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html',
+         form_class=UserLoginForm), name='login'),
+
     path('register/', views.account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>', views.account_activate, name='activate'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+
 ]
