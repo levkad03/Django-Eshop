ccb063a0d081d54a7905b22fd3b53e117e7acff7
Lev Kadatskyi <levkadatscky@gmail.com>
2024-02-15 15:16:04 +0200

    added client side submission of the payment

--- payment/views.py
+++ payment/views.py
@@ -8,17 +8,19 @@ from basket.basket import Basket
 
 @login_required
 def BasketView(request):
-    
+
     basket = Basket(request)
     total = str(basket.get_total_price())
     total = total.replace(".", "")
     total = int(total)
-    
+
+    print(total)
+
     stripe.api_key = settings.STRIPE_SECRET_KEY
     intent = stripe.PaymentIntent.create(
         amount=total,
         currency='gbp',
         metadata={'user_id': request.user.id}
     )
-    
+
     return render(request, "payment/home.html", {"client_secret": intent.client_secret})
--- static/payment/index.js
+++ static/payment/index.js
@@ -1,5 +1,5 @@
 var stripe = Stripe(
-  "pk_test_51Ok36zGN46pmt5AWmxaq5XDGZvjDorLrw5PLiFhoVK00N1mNKOLrtUsgXqZSaXn6tXZSEepRhN3MBaPDlRuLRI8x00eY9l9qsO"
+    "pk_test_51Ok36zGN46pmt5AWmxaq5XDGZvjDorLrw5PLiFhoVK00N1mNKOLrtUsgXqZSaXn6tXZSEepRhN3MBaPDlRuLRI8x00eY9l9qsO"
 );
 
 var elem = document.getElementById("submit");
@@ -8,23 +8,61 @@ clientsecret = elem.getAttribute("data-secret");
 // Setup Stripe.js and Elements
 var elements = stripe.elements();
 var style = {
-  base: {
-    color: "#000",
-    lineHeight: "2.4",
-    fontSize: "16px",
-  },
+    base: {
+        color: "#000",
+        lineHeight: "2.4",
+        fontSize: "16px",
+    },
 };
 
 var card = elements.create("card", { style: style });
 card.mount("#card-element");
 
 card.on("change", function (event) {
-  var displayError = document.getElementById("card-errors");
-  if (event.error) {
-    displayError.textContent = event.error.message;
-    $("#card-errors").addClass("alert alert-info");
-  } else {
-    displayError.textContent = "";
-    $("#card-errors").removeClass("alert alert-info");
-  }
+    var displayError = document.getElementById("card-errors");
+    if (event.error) {
+        displayError.textContent = event.error.message;
+        $("#card-errors").addClass("alert alert-info");
+    } else {
+        displayError.textContent = "";
+        $("#card-errors").removeClass("alert alert-info");
+    }
+});
+
+var form = document.getElementById("payment-form");
+
+form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+
+    var custName = document.getElementById("custName").value;
+    var custAdd = document.getElementById("custAdd").value;
+    var custAdd2 = document.getElementById("custAdd2").value;
+    var postCode = document.getElementById("postCode").value;
+
+    stripe.confirmCardPayment(clientsecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                address: {
+                    line1: custAdd,
+                    line2: custAdd2
+                },
+                name: custName
+            },
+        }
+    }).then(function (result) {
+        if (result.error) {
+            console.log('payment error')
+            console.log(result.error.message);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                console.log('payment processed')
+                // There's a risk of the customer closing the window before callback
+                // execution. Set up a webhook or plugin to listen for the
+                // payment_intent.succeeded event that handles any business critical
+                // post-payment actions.
+                window.location.replace("http://127.0.0.1:8000/payment/orderplaced/");
+            }
+        }
+    });
 });
