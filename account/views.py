64a5ae1e86c64e43464303dede45b89ff12afa44
Lev Kadatskyi <levkadatscky@gmail.com>
2024-02-17 15:22:29 +0200

    updated dashboard, added user orders in dashboard

--- account/views.py
+++ account/views.py
@@ -7,6 +7,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+from orders.views import user_orders
 from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .token import account_activation_token
@@ -14,8 +15,9 @@ from .token import account_activation_token
 
 @login_required
 def dashboard(request):
+    orders = user_orders(request)
     return render(request,
-                  'account/user/dashboard.html')
+                  'account/user/dashboard.html', {'orders': orders})
 
 
 def account_register(request):
